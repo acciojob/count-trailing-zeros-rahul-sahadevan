cf722711408cd2554339c85ca9f509e97811cb30
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-12-10 19:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function trailingZeros(n) {
-  //your JS code here. If required.
+	let count =0;
+  
+  let fact = 1;
+  
+  for(let i =1;i<=n;i++){
+    fact *= i;
+  }
+  let i =0
+  
+  while(fact > 0){
+    let rem = fact % 10;
+    if(rem > 0){
+      return count;
+    }
+    else{
+      count++;
+    }
+    
+    fact = fact / 10;
+    i++;
+  }
+	
 }
 
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
+
